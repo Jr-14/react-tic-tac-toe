f3b253c2d5e5d30c354d9f22a18c623aef5849af
John Requizo <jr.jemerson14@gmail.com>
2023-03-12 16:21:16 +1100

    Add board and buttons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello Tic Tac Toe</h1>
+      <Board />
     </div>
   );
+}
+
+function Board() {
+  return (
+    <div className="board">
+      <div classname="board-row">
+        <Square value="0" />
+        <Square value="0" />
+        <Square value="0" />
+      </div>
+      <div classname="board-row">
+        <Square value="0" />
+        <Square value="0" />
+        <Square value="0" />
+      </div>
+      <div classname="board-row">
+        <Square value="0" />
+        <Square value="0" />
+        <Square value="0" />
+      </div>
+    </div>
+  );
+}
+
+function Square({ value }) {
+  return (
+    <button>{ value }</button>
+  )
 }
 
 export default App;
